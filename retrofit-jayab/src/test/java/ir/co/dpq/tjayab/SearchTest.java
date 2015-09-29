3aba636eba22c8ee310d50060dcb81f7218927e6
mostafa barmshory <mostafa.barmshory@dpq.co.ir>
2015-09-29 14:42:47 +0330

    تست جستجو هم اضافه شده است.

--- retrofit-jayab/src/test/java/ir/co/dpq/tjayab/SearchTest.java
+++ retrofit-jayab/src/test/java/ir/co/dpq/tjayab/SearchTest.java
@@ -1,22 +1,30 @@
 package ir.co.dpq.tjayab;
 
-import ir.co.dpq.jayab.ILocationService;
-import ir.co.dpq.jayab.Location;
-import ir.co.dpq.jayab.Tag;
-import ir.co.dpq.pluf.PErrorHandler;
-import ir.co.dpq.pluf.PPaginatorPage;
-import ir.co.dpq.pluf.user.IPUserService;
-import ir.co.dpq.pluf.user.PUser;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
 
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import ir.co.dpq.jayab.ILocationService;
+import ir.co.dpq.jayab.Location;
+import ir.co.dpq.jayab.Tag;
+import ir.co.dpq.pluf.DeserializerJson;
+import ir.co.dpq.pluf.PErrorHandler;
+import ir.co.dpq.pluf.PPaginatorPage;
+import ir.co.dpq.pluf.user.IPUserService;
+import ir.co.dpq.pluf.user.PUser;
 import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
 
 public class SearchTest {
 
@@ -28,8 +36,18 @@ public class SearchTest {
 		CookieManager cookieManager = new CookieManager();
 		cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
 		CookieHandler.setDefault(cookieManager);
+		
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder
+			/*
+			 * این تبدیل برای صفحه بندی به کار گرفته می‌شود.
+			 */
+			.registerTypeAdapter(PPaginatorPage.class, new DeserializerJson());
+		Gson gson = gsonBuilder.create();
 
 		RestAdapter restAdapter = new RestAdapter.Builder()
+				// تعیین مبدل داده
+				.setConverter(new GsonConverter(gson))
 				// تعیین کنترل کننده خطا
 				.setErrorHandler(new PErrorHandler())
 				// تعیین آدرس سایت مورد نظر
@@ -41,16 +59,17 @@ public class SearchTest {
 	}
 
 	@Test
-	public void findPlace() {
-//		PUser user = userService.login("test", "test");
-//		assertNotNull(user);
-//		assertTrue(user.isActive());
-//		assertEquals("test", user.getLogin());
-//
-//		PPaginatorPage<Location> places = jayabService.findLocation(0.0, 0.0, 10, 1000.0, Tag.Key.AMENITY,
-//				Tag.Value.PARKING);
-//
-//		assertNotNull(places);
-//		assertFalse(places.isEmpty());
+	public void findPlace00() {
+		PUser user = userService.login("admin", "admin");
+		assertNotNull(user);
+		assertTrue(user.isActive());
+		assertEquals("admin", user.getLogin());
+
+		PPaginatorPage<Location> places = jayabService.findLocation(0.1, 0.1, 10, 1000.0, Tag.Key.AMENITY,
+				Tag.Value.PARKING);
+
+		assertNotNull(places);
+		assertFalse(places.isEmpty());
+		assertEquals(places.getCounts(), places.getItems().size());
 	}
 }
